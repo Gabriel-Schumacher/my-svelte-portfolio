cca67e849558acb4b2329d538fb537b32a6678bd
Gabriel-Schumacher <schumachergabe@gmail.com>
2025-05-15 11:28:35 -0600

    Cards, dark mode

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -4,7 +4,7 @@ export let buttonText: string = 'Project Description';
 export let image: string = 'https://via.placeholder.com/150';
 </script>
 
-<a  href="{link}" target="_blank" class="card shadow-xl hover:shadow-2xl bg-surface-50 rounded-lg p-4 flex flex-col gap-4 border-1 border-gray-200 md:max-h-[28rem]">
+<a  href="{link}" target="_blank" class="card shadow-xl hover:shadow-2xl bg-surface-50 rounded-lg p-4 flex flex-col gap-4 border-1 border-gray-200 md:max-h-[28rem] dark:bg-surface-900">
     <div class="items-center self-center">
         <img src="{image}" alt="Project">        
     </div>
